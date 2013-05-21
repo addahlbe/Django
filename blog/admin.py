f1708a411c4df2ee15a4bfd45ae4ada9ba4bb9ba
Anthony Dahlberg <anthony-dahlberg@uiowa.edu>
2013-05-21 15:56:59 -0500

    First real commit

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from blog.models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description']
+    list_filter = ['published', 'created']
+    search_fields = ['title', 'description', 'content']
+    date_hierachy = 'created'
+    save_on_top = True
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Post, PostAdmin)
