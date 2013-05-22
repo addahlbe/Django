9fdbd0fd74d721982ff52a26c7dc25b41449b024
Anthony Dahlberg <anthony-dahlberg@uiowa.edu>
2013-05-22 12:15:03 -0500

    progress on tut

--- netmag/urls.py
+++ netmag/urls.py
@@ -6,15 +6,17 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
+    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
     url(r'^$', 'blog.views.index'),
     url(r'^(?P<slug>[\w\-]+)/$', 'blog.views.post'),
+
     # url(r'^$', 'netmag.views.home', name='home'),
     # url(r'^netmag/', include('netmag.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
